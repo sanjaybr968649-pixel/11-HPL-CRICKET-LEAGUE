7727381e1dfc9c8766800c19dcf9976e17aae42e
sanjaybr968649-pixel <sanjay.br968649@gmail.com>
2026-09-19 23:05:46 +0530

    Initialize Firebase and Firestore in config
    Added Firebase initialization and Firestore setup.

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,6 @@
-// Firebase configuration for HPL Season 11
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDsvWDbT7GlvtWtoR8o4N-jLEP4rC8oGcA",
   authDomain: "hand-cricket-primier-league.firebaseapp.com",
@@ -8,3 +10,8 @@ const firebaseConfig = {
   appId: "1:270593215152:web:7502fb60432042b6258975",
   measurementId: "G-9S1WS03TBH"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
